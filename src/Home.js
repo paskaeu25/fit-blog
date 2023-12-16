694bf285fbfa72d0617cbf8d538ccda874920447
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-12-16 11:31:08 +0200

    played with click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+  let times = 0;
+  const handleClick = (e) => {
+    times += 1;
+    console.log(`button clicked ${times} times.`, e);
+  };
+
+  const handleClickMeAgain = (name, e) => {
+    console.log(`clicked ${name}`, e.target);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickMeAgain('pasha', e)}>
+        Click Me Again
+      </button>
     </div>
   );
 };
