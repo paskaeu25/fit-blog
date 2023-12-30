2285df6a474f36748e5eed71b8bac713db405aba
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-12-30 14:20:59 +0200

    added usefetch hook to blog details component

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -7,28 +7,29 @@ const useFetch = (url) => {
 
   useEffect(() => {
     const abortCont = new AbortController();
-    setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error('Bad request url');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
+    // use setTimeout for fetch delay to demonstrate loading message.
+    // setTimeout(() => {
+    // }, 1000);
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Bad request url');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted');
+        } else {
           setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          if (err.name === 'AbortError') {
-            console.log('fetch aborted');
-          } else {
-            setIsPending(false);
-            setError(err.message);
-          }
-        });
-    }, 1000);
+          setError(err.message);
+        }
+      });
 
     return () => abortCont.abort();
   }, [url]);
